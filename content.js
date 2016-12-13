ee623bf24aa58b65ed885922e31ae27d474b1d79
Minsup Ko <atsundwn@gmail.com>
2016-12-13 01:10:40 -0500

    somewhat works

--- content.js
+++ content.js
@@ -1,58 +1,48 @@
-alert("Hello from your Chrome extension!");
-
-var list_commit_messages = $('.subject, .commit-message');
-
-console.log(list_commit_messages);
-list_commit_messages.each(function()
+function doTask()
 {
-    var $this = $(this);
-    var text = $this.text();
+    var list_messages = $('.subject, .commit-message');
 
-
-    var start1 = text.search(/(CORE\-\d\d\d\d|core\-\d\d\d\d)/);
-    var start2 = text.search(/(CS\-\d\d\d\d|cs\-\d\d\d\d)/);
-    //var start3 = text.search(/(TEMOB\-\d\d\d|temob\-\d\d\d)/);
-    var start3 = null;
-    var start4 = text.search(/(BEMOB\-\d\d\d\d|bemob\-\d\d\d\d)/);
-    var start5 = text.search(/(MAINT\-\d\d\d\d|maint\-\d\d\d\d)/);
-
-    if(start1 > -1)
+    list_messages.each(function()
     {
-        replaceLink(start1, 9);
-        start3 = text.search(/(TEMOB\-\d\d\d|temob\-\d\d\d)/);
-    }
+        var $this = $(this);
+        var text = $this.text();
+        var filter = [
+                         ['(CORE\\-\\d\\d\\d\\d|core\\-\\d\\d\\d\\d)', 9]
+                        ,['(CS\\-\\d\\d\\d\\d|cs\\-\\d\\d\\d\\d)', 7]
+                        ,['(BEMOB\\-\\d\\d\\d\\d|bemob\\-\\d\\d\\d\\d)', 10]
+                        ,['(BEMOB\\-\\d\\d\\d\\D|bemob\\-\\d\\d\\d\\D)', 9]
+                        ,['(TEMOB\\-\\d\\d\\d\\d|temob\\-\\d\\d\\d\\d)', 10]
+                        ,['(TEMOB\\-\\d\\d\\d\\D|temob\\-\\d\\d\\d\\D)', 9]
+                        ,['(MAINT\\-\\d\\d\\d\\d|maint\\-\\d\\d\\d\\d)', 10]
+                        ,['(MAINT\\-\\d\\d\\d[\\D\\S]|maint\\-\\d\\d\\d)', 9]
+                        ,['(DEP\\-\\d\\d\\d|dep\\-\\d\\d\\d)', 7]
+                     ];
 
-    if(start2 > -1)
-    {
-        replaceLink(start2, 7);
-    }
+        for(var i=0,l=filter.length; i < l; i++)
+        {
+            var regex = new RegExp(filter[i][0]);
+            var start = text.search(regex);
 
+            if(start > -1)
+            {
+                replaceLink(start, filter[i][1]);
+            }
+        };
 
-    console.log(start3);
-    if(start3 > -1)
-    {
-        replaceLink(start3, 9);
-    }
+        $this.html(text);
 
-    if(start4 > -1)
-    {
-        replaceLink(start4, 10);
-    }
+        function replaceLink(start, length)
+        {
+            var ticket = text.substr(start, length);
+            text = text.replace(ticket, '<a href=http://issues.buildingengines.com/browse/' + ticket + '>' + ticket + '</a>');
+        };
+    });
+};
 
+$(document).on('click', function()
+{
+    window.setTimeout(doTask(), 2000);
 
-    if(start4 > -1)
-    {
-        replaceLink(start4, 10);
-    }
-
-    $this.html(text);
-
-    function replaceLink(start, length)
-    {
-        var ticket = text.substr(start, length);
-        text = text.replace(ticket, '<a href=http://google.com>' + ticket + '</a>');
-    }
 
 });
-
-
+doTask();
